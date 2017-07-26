ddde47d791c22b5fb527b49c4240dc1d5b59ca99
Fernando Carvajal <efcu93@gmail.com>
2017-07-26 13:35:05 -0500

    asdsadsadasda

---
+++ TheCircle/Models/Institucion.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TheCircle.Models
+{
+    public class Institucion
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string especialidad { get; set; }
+        public string genero { get; set; }
+        public double costo { get; set; }
+        public int edadInicial { get; set; }
+        public int edadFinal { get; set; }
+
+        public Institucion() { }
+    }
+}
