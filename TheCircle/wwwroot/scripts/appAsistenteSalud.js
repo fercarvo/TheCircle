b8e5ddfaed70976515b34ed364460d6ebac38063
Fernando Carvajal <efcu93@gmail.com>
2017-08-10 13:52:03 -0500

    se termino despachar

--- TheCircle/wwwroot/scripts/appAsistenteSalud.js
+++ TheCircle/wwwroot/scripts/appAsistenteSalud.js
@@ -39,21 +39,50 @@
     .controller('despachar', ["$scope", "$state", "$http", "dataFac", function ($scope, $state, $http, dataFac) {
         $scope.recetas = dataFac.recetas;
         $scope.receta = null;
+        $scope.despachado = [];
 
         if (dataFac.recetas === null) {
             dataFac.getRecetas(dataFac.localidad).then(function success(res) {
                 dataFac.recetas = res.data;
                 $scope.recetas = dataFac.recetas;
+
             }, function error(err) {
                 console.log("error cargar recetas");
             })
         };
 
         $scope.select = function (receta) {
+            receta.items.map(function (item) {
+                item.desactivar = true;
+            });
             $scope.receta = receta;
 
         }
-        
+
+        $scope.activar = function (item) {
+            item.desactivar = false;
+        }
+
+        $scope.despachar = function (item) {
+            var itemDespachado = angular.copy(item);
+            $scope.despachado.push(itemDespachado);
+        }
+
+        $scope.guardarDespacho = function () {
+            if ($scope.despachado.length == 0 || !$scope.despachado) {
+                alert("no hay items a despachar");
+            } else if ($scope.despachado.length != $scope.receta.items.length) {
+                alert("La cantidad de items a despachar son diferentes a la receta")
+            } else if ($scope.despachado.length == $scope.receta.items.length) {
+                $scope.receta = null;
+                $scope.despachado = [];
+                alert("despacho exitoso");
+            }
+        }
+
+        $scope.limpiarDespacho = function () {
+            $scope.despachado = [];
+        }
 
     }])
     .controller('historial', ["$scope", "$state", "$http", function ($scope, $state, $http) {
