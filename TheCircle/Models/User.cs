516b20c4ce396b669bb7f676d91325350ece3446
Fernando Carvajal <efcu93@gmail.com>
2017-08-25 17:28:22 -0500

    asdsadasfd

---
+++ TheCircle/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace TheCircle.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string cargo { get; set; }
+        public int cedula { get; set; }
+
+        public User isValid(LoginRequest req, MyDbContext _context)
+        {
+            string query = $"EXEC dbo.select_User @cedula={req.cedula}, @clave='{req.clave}'";
+            try
+            {
+                var data = _context.User.FromSql(query).First();
+                return data;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error cargar User, User.get");
+            }
+
+        }
+
+    }
+}
