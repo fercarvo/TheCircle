2cd869b869eb4223d971da1b5e425d390c4d25b7
aespinosa <aespinosa@GUY130>
2017-08-28 15:04:08 -0500

    cambios del lunes 28/8/2017 alejandro

--- TheCircle/wwwroot/scripts/appBodeguero.js
+++ TheCircle/wwwroot/scripts/appBodeguero.js
@@ -27,3 +27,38 @@
     .controller('historial', ["$log", "$scope", "$state", "$http", function ($log, $scope, $state, $http) {
         $scope.casa = "dasdasdasd"
     }])
+    .controller('ingresar', ["$scope", "$http", "dataFac", "notify", "date", function ($scope, $http, dataFac, notify, date) {
+        $scope.compuestos = dataFac.compuestos;
+        $scope.items = null;
+        dataFac.getCompuestos();
+        //var actualizar = refresh.go(cargar, 30000);
+
+        $scope.$on('dataFac.compuestos', function () {
+            $scope.compuestos = dataFac.compuestos;
+        })
+
+        $scope.crear = function (compuesto, item, fecha, cantidad) {
+            var data = {
+                compuesto: compuesto,
+                nombre: item,
+                fcaducidad: date(fecha),
+                cantidad: cantidad
+            }
+            console.log("data a enviar", data);
+
+            $http.post("api/itemfarmacia", data).then(function sucess(res) {
+                console.log("Ingreso exitoso", res.data);
+                notify("Ingreso en farmacia exitoso", "success");
+                compuesto = item = fecha = cantidad = {};
+            }, function err(err) {
+                console.log("No se pudo guardar el ingreso", err)
+                notify("No se ha podido guardar el ingreso en farmacia", "danger");
+            })
+        }
+
+        $scope.cambioCompuesto = function (items) {
+            $scope.items = items;
+        }
+
+
+    }])
--- TheCircle/wwwroot/scripts/appSistema.js
+++ TheCircle/wwwroot/scripts/appSistema.js
@@ -1,14 +1,34 @@
 ﻿angular.module('appSistema', ['ui.router'])
     .config(["$stateProvider", function ($stateProvider) {
         $stateProvider
-            .state('personal', {
-                templateUrl: 'views/sistema/personal.html',
-                controller: 'personal'
+            .state('activarusuario', {
+                templateUrl: 'views/sistema/activarusuario.html',
+                controller: 'activarusuario'
+            });
+    }])
+    .config(["$stateProvider", function ($stateProvider) {
+        $stateProvider
+            .state('desactivarusuario', {
+                templateUrl: 'views/sistema/desactivarusuario.html',
+                controller: 'desactivarusuario'
+            });
+    }])
+    .config(["$stateProvider", function ($stateProvider) {
+        $stateProvider
+            .state('modificarusuario', {
+                templateUrl: 'views/sistema/modificarusuario.html',
+                controller: 'modificarusuario'
             });
     }])
     .run(["$state", function ($state) {
-        $state.go("personal");
+        $state.go("activarusuario");
     }])
-    .controller('personal', ["$scope", "$state", "$http", function ($scope, $state, $http) {
+    .controller('activarusuario', ["$scope", "$state", "$http", function ($scope, $state, $http) {
+        $scope.casa = "casacasacasa?";
+    }])
+    .controller('desactivarusuario', ["$scope", "$state", "$http", function ($scope, $state, $http) {
+        $scope.casa = "casacasacasa?";
+    }])
+    .controller('modificarusuario', ["$scope", "$state", "$http", function ($scope, $state, $http) {
         $scope.casa = "casacasacasa?";
     }])
