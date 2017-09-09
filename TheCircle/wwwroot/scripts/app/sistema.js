363935aeacb3be13584d8fd13e9da20c1b083bd1
Fernando Carvajal <efcu93@gmail.com>
2017-09-09 18:53:32 -0500

    minify

--- TheCircle/wwwroot/scripts/app/sistema.js
+++ TheCircle/wwwroot/scripts/app/sistema.js
@@ -1,4 +1,8 @@
-﻿/*sistema v1.0 - Edgar Fernando Carvajal Ulloa efcarvaj@espol.edu.ec - Children International*/
+﻿/*
+    sistema v1.0 
+    Edgar Fernando Carvajal Ulloa efcarvaj@espol.edu.ec
+    Children International
+*/
 angular.module('sistema', ['ui.router', 'ngCookies'])
     .config(["$stateProvider", "$compileProvider", function ($stateProvider, $compileProvider) {
         $stateProvider
@@ -14,10 +18,14 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
                 templateUrl: 'views/sistema/cambiarclave.html',
                 controller: 'cambiarclave'
             });
-        //$compileProvider.debugInfoEnabled(false); //Activar en modo producción
+        $compileProvider.debugInfoEnabled(true); //False en modo de produccion
     }])
     .run(["$state", "$rootScope", "$cookies", "$http", "refresh", function ($state, $rootScope, $cookies, $http, refresh) {
 
+        /*
+        Cada X segundos se ejecuta un HTTP GET a /login con el fin de determinar 
+        si esta activa o no la session, en caso de estar inactiva se redirecciona /logout
+        */
         refresh.goTime(function () {
             $http.get("login").then(function () {
             }, function (response) {
@@ -28,6 +36,7 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             })
         }, 1000 * 60 * 20)
 
+        //Se carga desde los cookies el nombre de la persona con la session
         $rootScope.session_name = (function () {
             var c = $cookies.get('session_name')
             if (c) {
@@ -35,6 +44,7 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             } return ""
         })()
 
+        //Se carga desde las cookies el email de la persona con la session
         $rootScope.session_email = (function () {
             var c = $cookies.get('session_email')
             if (c) {
@@ -42,6 +52,7 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             } return ""
         })()
 
+        //Se carga desde las cookies el link para obtener la foto de la persona con la session
         $rootScope.session_photo = (function () {
             var c = $cookies.get('session_photo')
             if (c) {
@@ -49,24 +60,25 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             } return "/images/ci.png"
         })()
 
+        //Esta inicial de la app web
         $state.go("activarusuario");
     }])
     .factory('refresh', [function () { //Sirve para ejecutar una funcion cada cierto tiempo y detenerla cuando se requiera.
 
         function go(fn) {
             fn();
-            console.log("Go refresh");
+            //console.log("Go refresh");
             return setInterval(fn, 10000);
         }
 
         function goTime(fn, time) {
             fn();
-            console.log("Go refresh by ", time);
+            //console.log("Go refresh by ", time);
             return setInterval(fn, time);
         }
 
         function stop(repeater) {
-            console.log("Stop refresh");
+            //console.log("Stop refresh");
             clearInterval(repeater);
         }
 
@@ -76,6 +88,19 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             goTime: goTime
         }
     }])
+    /**
+     * @description
+     *
+     * Esta factory retorna una funcin encargada de mostrar las notificaciones
+     * de la aplicacion
+     *
+     * @param {mensaje} Mensaje que será mostrado en la notificacion
+     * @param {tipo} success o danger, dependiendo de esos dos parametros cambiará de color.
+     * @returns {promise} Promise con la cual se puede terminar la notificación antes de que esta
+     * culmine por ejm
+     * var not = notify.close();
+     * not();
+     */
     .factory('notify', [function () {
         return function (mensaje, tipo) {
 
@@ -124,6 +149,13 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
                 })
         }
     }])
+        /**
+     * @description
+     *
+     * Esta factory un objeto que contiene una serie de información a guardar, asi como funciones
+     * a ser utilizadas por la app
+     *
+     */
     .factory('usuarios', ['$http', '$rootScope', function ($http, $rootScope) {
 
         var usuarios = {
@@ -135,33 +167,37 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             getAll: getAll
         }
 
+        //Se obtienen todos los usuarios inactivos que existen en el sistema
         function getInactivos() {
             $http.get("api/user/inactivos").then(function success(res) {
-                console.log("usuarios.inactivos", res.data);
+                //console.log("usuarios.inactivos", res.data);
                 usuarios.inactivos = res.data;
                 $rootScope.$broadcast('usuarios.inactivos'); //Se informa a los controladores que cambio usuarios.inactivos
             }, function error(err) {
-                console.log("error cargar usuarios.inactivos", err);
+                //console.log("error cargar usuarios.inactivos", err);
             })
         }
 
+        //Se obtienen todos los usuarios activos que existen en el sistema
         function getActivos() {
             $http.get("api/user/activos").then(function success(res) {
-                console.log("usuarios.activos", res.data);
+                //console.log("usuarios.activos", res.data);
                 usuarios.activos = res.data;
                 $rootScope.$broadcast('usuarios.activos'); //Se informa a los controladores que cambio usuarios.activos
             }, function error(err) {
-                console.log("error cargar usuarios.activos", err);
+                //console.log("error cargar usuarios.activos", err);
             })
         }
 
+        //Se obtienen todos los usuarios activos y no activos del sistema, excepto los que tienen
+        //Como rol "sistema"
         function getAll() {
             $http.get("api/user").then(function success(res) {
-                console.log("usuarios.all", res.data);
+                //console.log("usuarios.all", res.data);
                 usuarios.all = res.data;
                 $rootScope.$broadcast('usuarios.all'); //Se informa a los controladores que cambio usuarios.all
             }, function error(err) {
-                console.log("error cargar usuarios.all", err);
+                //console.log("error cargar usuarios.all", err);
             })
         }
 
@@ -190,12 +226,12 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             $('#modal_activar').modal('hide');
             NProgress.start();
             $http.put("api/user/" + $scope.usuario.cedula + "/activar").then(function success(res) {
-                console.log("Activado con exito");
+                //console.log("Activado con exito");
                 $scope.usuarios = res.data;
                 notify("Usuario activado exitosamente", "success");
                 NProgress.done();
             }, function error(err) {
-                console.log("error al activar");
+                //console.log("error al activar");
                 notify("No se ha podido activar el usuario", "danger");
                 NProgress.done();
             })
@@ -224,18 +260,18 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             $('#modal_desactivar').modal('hide');
             NProgress.start();
             $http.put("api/user/" + $scope.usuario.cedula + "/desactivar").then(function success(res) {
-                console.log("desActivado con exito");
+                //console.log("desActivado con exito");
                 $scope.usuarios = res.data;
                 notify("No se ha podido activar el usuario", "success");
                 NProgress.done();
             }, function error(err) {
-                console.log("error al desactivar");
+                //console.log("error al desactivar");
                 notify("No se ha podido activar el usuario", "danger");
                 NProgress.done();
             })
         }
     }])
-    .controller('cambiarclave', ["$scope", "$state", "$http", "usuarios", function ($scope, $state, $http, usuarios) {
+    .controller('cambiarclave', ["$scope", "$state", "$http", "usuarios", "notify", function ($scope, $state, $http, usuarios, notify) {
         $scope.usuarios = usuarios.all;
         $scope.usuario = null;
         $scope.clave = null;
@@ -255,12 +291,12 @@ angular.module('sistema', ['ui.router', 'ngCookies'])
             NProgress.start();
             $http.put("api/user/" + $scope.usuario.cedula + "/clave/set").then(function success(res) {
                 NProgress.done();
-                console.log("Cambio de clave exitoso", res.data);
+                //console.log("Cambio de clave exitoso", res.data);
                 $scope.clave = res.data.clave;
                 $('#nueva_clave').modal('show');
                 notify("Reseteo de clave exitoso", "success");
             }, function error(err) {
-                console.log("error cambio de clave");
+                //console.log("error cambio de clave");
                 NProgress.done();
                 notify("No se ha podido cambiar la clave del usuario", "danger");
             })
